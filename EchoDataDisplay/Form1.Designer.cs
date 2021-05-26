9e5ae9d876a0c90699f59687f8ff5def423f8639
joeljohn49 <joeljohn49@gmail.com>
2021-05-26 13:11:30 +1000

    Add project files.

---
+++ EchoDataDisplay/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace EchoDataDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFile1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFile2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.createOutput = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFile1
+            // 
+            this.openFile1.Location = new System.Drawing.Point(45, 84);
+            this.openFile1.Name = "openFile1";
+            this.openFile1.Size = new System.Drawing.Size(75, 24);
+            this.openFile1.TabIndex = 0;
+            this.openFile1.Text = "Open File";
+            this.openFile1.UseVisualStyleBackColor = true;
+            this.openFile1.Click += new System.EventHandler(this.openFile1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(551, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFile2
+            // 
+            this.openFile2.Location = new System.Drawing.Point(45, 214);
+            this.openFile2.Name = "openFile2";
+            this.openFile2.Size = new System.Drawing.Size(75, 24);
+            this.openFile2.TabIndex = 2;
+            this.openFile2.Text = "Open File";
+            this.openFile2.UseVisualStyleBackColor = true;
+            this.openFile2.Click += new System.EventHandler(this.openFile2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(551, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // createOutput
+            // 
+            this.createOutput.Location = new System.Drawing.Point(293, 353);
+            this.createOutput.Name = "createOutput";
+            this.createOutput.Size = new System.Drawing.Size(75, 23);
+            this.createOutput.TabIndex = 4;
+            this.createOutput.Text = "Create File";
+            this.createOutput.UseVisualStyleBackColor = true;
+            this.createOutput.Click += new System.EventHandler(this.createOutput_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select the First Sensor Text File\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Select the Second Sensor Text File\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 451);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createOutput);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.openFile2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openFile1);
+            this.Name = "Form1";
+            this.Text = "Echo Sounder Data Merger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFile1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button openFile2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button createOutput;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ EchoDataDisplay/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EchoDataDisplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Open First Sensor log File";
+            openFileDialog1.Filter = "log files (*.log)|*.log|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.ShowDialog();
+
+            string file1Path = openFileDialog1.FileName;
+
+            textBox1.Text = file1Path;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFile2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Title = "Open Second Sensor log File";
+            openFileDialog2.Filter = "log files (*.log)|*.log|All files (*.*)|*.*";
+            openFileDialog2.FilterIndex = 1;
+            openFileDialog2.ShowDialog();
+
+            string file2Path = openFileDialog2.FileName;
+
+            textBox2.Text = file2Path;
+        }
+
+        private void createOutput_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Choose File Destination";
+            saveFileDialog1.DefaultExt = "csv";
+            saveFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+
+            saveFileDialog1.ShowDialog();
+
+            string saveFilePath = saveFileDialog1.FileName;
+
+
+            string[] lines1 = System.IO.File.ReadAllLines(textBox1.Text);
+            string[] lines2 = System.IO.File.ReadAllLines(textBox2.Text);
+
+            var timeStampList = new List<string>();
+            var waterTempList = new List<string>();
+            var waterDepth1List = new List<string>();
+            var waterDepth2List = new List<string>();
+            var latLongList = new List<string>();
+
+            foreach (string line in lines1)
+            {
+                string[] checksumRemoved = line.Split(new[] { '*' }, 2);
+                string[] splitLine = checksumRemoved[0].Split(new[] { ',' });
+
+                switch (splitLine[0])
+                {
+                    case "$SDZDA":
+                        {
+                            string jointLine = splitLine[1] + "," + splitLine[2] + "," + splitLine[3] + "," + splitLine[4];
+                            timeStampList.Add(jointLine);
+                            break;
+                        }
+                    case "$SDMTW":
+                        {
+                            string jointLine = splitLine[1] + splitLine[2];
+                            waterTempList.Add(jointLine);
+                            break;
+                        }
+                    case "$SDDBT":
+                        {
+                            string jointLine = splitLine[1] + splitLine[2] + "," + splitLine[3] + splitLine[4];
+                            waterDepth1List.Add(jointLine);
+                            break;
+                        }
+                    case "$GNGGA":
+                        {
+                            string jointLine = splitLine[2] + splitLine[3] + "," + splitLine[4] + splitLine[5];
+                            latLongList.Add(jointLine);
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+
+            foreach (string line in lines2)
+            {
+                if (line.StartsWith("$SDDBT"))
+                {
+                    string[] checksumRemoved = line.Split(new[] { '*' }, 2);
+                    string[] splitLine = checksumRemoved[0].Split(new[] { ',' });
+                    string jointLine = splitLine[1] + splitLine[2] + "," + splitLine[3] + splitLine[4];
+                    waterDepth2List.Add(jointLine);
+                }
+            }
+
+            // Set a variable to the Documents path.
+            //string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(saveFilePath))
+            {
+                outputFile.WriteLine("Time (hhmmss.ss),Day,Month,Year,Water Temp (C),Depth 1 (ft),Depth 1 (m),Depth 2 (ft),Depth 2 (m),Latitude,Longitude");
+                for (int i = 0; i < timeStampList.Count; i++)
+                {
+                    string row = timeStampList[i] + "," + waterTempList[i] + "," + waterDepth1List[i] + "," + waterDepth2List[i] + "," + latLongList[i];
+                    outputFile.WriteLine(row);
+                }
+            }
+            MessageBox.Show("Output File Created", "Save Successful");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
